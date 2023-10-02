6ea09d0719b96b276784f57e7059f0c522819a34
Marcin <marcinbaran789@gmail.com>
2023-10-02 18:31:12 +0200

    ResponsiveMenu

---
+++ main.js
@@ -0,0 +1,12 @@
+const dropdownMenu=document.querySelector(".dropdown_menu")
+const menuBtn=document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click",function(){
+    dropdownMenu.classList.toggle("active");
+    
+    
+       
+})
+
+
+
